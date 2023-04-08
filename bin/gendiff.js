402263490952573bc7770e9fd7fd825fc52ecff1
BOMBYASCHER <bombyascher@gmail.com>
2023-04-08 16:46:49 +0300

    Add logic to gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
-import { program } from "commander";
+import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
 
-program.parse();
+program.parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+import { cwd } from 'node:process';
+import _ from 'lodash';
+
+const genDiff = (filepath1, filepath2) => {
+  const file1 = JSON.parse(readFileSync(path.resolve(cwd(), filepath1)));
+  const file2 = JSON.parse(readFileSync(path.resolve(cwd(), filepath2)));
+  const keys = _.union(Object.keys(file1), Object.keys(file2));
+
+  const data = keys.sort().map((key) => {
+    if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
+      if (file1[key] === file2[key]) {
+        return `${key}: ${file1[key]}`;
+      }
+      return [`- ${key}: ${file1[key]}`, `+ ${key}: ${file2[key]}`];
+    }
+    if (!Object.hasOwn(file1, key)) {
+      return `+ ${key}: ${file2[key]}`;
+    }
+    if (!Object.hasOwn(file2, key)) {
+      return `- ${key}: ${file1[key]}`;
+    }
+    return null;
+  }).flat();
+  const result = data.reduce((acc, info) => `${acc}\n  ${info}`, '');
+  return `{${result}\n}`;
+};
+
+export default genDiff;
