24f8315d7be0635c24c0ed96fd8b5822673b93cb
BOMBYASCHER <bombyascher@gmail.com>
2023-04-10 16:51:24 +0300

    Add test and update project

---
+++ __tests__/index.test.js
@@ -0,0 +1,21 @@
+import { beforeEach } from '@jest/globals';
+import genDiff from '../src/index.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'node:path';
+
+let correctPath1;
+let correctPath2;
+
+beforeEach(() => {
+  correctPath1 = '__fixtures__/file1.json';
+  correctPath2 = '__fixtures__/file2.json';
+})
+
+test('GenDiff check', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filepath1 = path.join(__dirname, '..', '__fixtures__', 'file1.json');
+  const filepath2 = path.join(__dirname, '..', '__fixtures__', 'file2.json');
+  expect(genDiff(filepath1, filepath2)).toEqual(genDiff(correctPath1, correctPath2));
+})
