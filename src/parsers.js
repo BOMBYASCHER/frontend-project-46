7b43559a659fd618c8aaffe72985efe901752f64
BOMBYASCHER <bombyascher@gmail.com>
2023-04-11 17:01:40 +0300

    Fix code

--- src/parsers.js
+++ src/parsers.js
@@ -4,14 +4,13 @@ import { cwd } from 'node:process';
 import { load } from 'js-yaml';
 
 const parseByFormat = (file) => {
-  let data;
+  const parsers = {
+    '.json': JSON.parse,
+    '.yml': load,
+    '.yaml': load,
+  };
   const format = path.extname(file);
-  if (format === '.json') {
-    data = JSON.parse(readFileSync(path.resolve(cwd(), file)));
-  }
-  if (format === '.yml' || format === '.yaml') {
-    data = load(readFileSync(path.resolve(cwd(), file)));
-  }
+  const data = parsers[format](readFileSync(path.resolve(cwd(), file)));
   return data;
 };
 
