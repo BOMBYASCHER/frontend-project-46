10c714a6dd1870d53d99b0f1b9922092e1bc586f
BOMBYASCHER <bombyascher@gmail.com>
2023-04-26 20:18:09 +0300

    Try to fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander';
-import genDiff from '../src/index.js';
+import genDiff from '../index.js';
 import chooseFormatter from '../src/formatters/index.js';
 
 program
